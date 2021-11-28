16fd021fe81ec4f5cb477602f234f1bfc3df45f3
Shyamin Ayesh <me@shyamin.com>
2021-11-29 00:16:47 +0530

    check access token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 export default function isAuthenticated(to, from, next) {
-    if (!localStorage.getItem("lgns")) {
+    if (!localStorage.getItem("access_token")) {
         return next({ name: "Login" })
     } else {
         (to.name == "Root") ? next({ name: "Home" }) : next()
