f470a667792de4f3226c3f2c271bff67db133625
Shyamin Ayesh <me@shyamin.com>
2021-11-29 00:14:20 +0530

    simple login

---
+++ src/store/modules/user.js
@@ -0,0 +1,41 @@
+export default {
+    namespaced: true,
+
+    state: {},
+
+    actions: {
+
+        login: (_, payload) => {
+            return new Promise((resolve, reject) => {
+
+                let formData = new FormData
+                formData.append("username", payload.username)
+                formData.append("password", payload.password)
+                formData.append("scope", "scope1")
+                formData.append("grant_type", "password")
+                formData.append("client_id", "622cc49f-6067-415e-82cd-04b1b76f6377")
+
+                fetch("https://omniscapp.slt.lk/mobitelint/slt/sltvasoauth/oauth2/token", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: new URLSearchParams(Object.fromEntries(formData)),
+                }).then(Response => { return ( Response.status == 200 ) ? Response.json() : reject(false) }).then(Response => {
+
+                    localStorage.setItem("access_token", Response.access_token)
+                    localStorage.setItem("refresh_token", Response.refresh_token)
+                    localStorage.setItem("subscriberid", Response.metadata)
+                    localStorage.setItem("consented_on", Response.consented_on)
+                    localStorage.setItem("expires_in", Response.expires_in)
+                    localStorage.setItem("refresh_token_expires_in", Response.refresh_token_expires_in)
+
+                    resolve(true)
+
+                }).catch(error => reject(error))
+
+            })
+        }
+
+    }
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,71 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid d-flex align-items-center vh-100">
+        <div class="col-3 mx-auto bg-white shadow">
+            <div class="card">
 
-        <div class="container">test</div>
+                <div class="card-header bg-white">
+                    <h3>Login</h3>
+                    <p>Enter your username and password to login</p>
+                </div>
 
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <b-form-input v-model="field.username" type="text" class="form-control input-lg" :state="validation" placeholder="943322XXXXX"></b-form-input>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <b-form-input v-model="field.password" type="text" class="form-control input-lg" :state="validation" placeholder="Password"></b-form-input>
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button v-on:click="this.login" class="btn btn-primary w-100 mt-3">Login</button>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
+    name: "Login",
+
+    data: () => ({
+        field: {
+            username: "",
+            password: ""
+        },
+        validation: null
+    }),
+
+    methods: {
+
+        login() {
+
+            this.validation = null
+
+            /**
+             * Dispatch request to the origin API and act according
+             * to the response.
+             */
+            this.$store.dispatch("user/login", {
+                username: this.field.username,
+                password: this.field.password
+            }).then(Response => {
+                if ( Response == true ) { this.$router.push("/home") } else { this.validation = false }
+            }).catch(() => this.validation = false )
+
+        }
+
+    }
+
 }
 </script>
+
+<style lang="scss" scoped>
+.container-fluid {
+    background-color: #f2f6ff;
+}
+</style>
