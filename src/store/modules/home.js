4902900a4e2b52bf3a5a013d7c5caad1ecbf5695
Shyamin Ayesh <me@shyamin.com>
2021-11-29 00:35:44 +0530

    gather connection summery

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,7 +1,40 @@
 export default {
     namespaced: true,
 
-    state: {},
+    state: {
+        package: {
+            name: null
+        }
+    },
 
-    actions: {}
+    actions: {
+
+        initialize: ({ commit }) => {
+            return new Promise((resolve, reject) => {
+
+                fetch("https://omniscapp.slt.lk/mobitelint/slt/sltvasservices/dashboard/summary", {
+                    method: "GET",
+                    headers: {
+                        "Authorization": "Bearer " + localStorage.getItem("access_token"),
+                        "subscriberid": localStorage.getItem("subscriberid"),
+                        "x-ibm-client-id": "622cc49f-6067-415e-82cd-04b1b76f6377"
+                    }
+                }).then(Response => Response.json()).then(Response => {
+                    commit("set_package", Response)
+                    resolve(true)
+                }).catch(() => reject(false))
+
+            })
+        }
+
+    },
+
+    mutations: {
+
+        set_package: (state, data) => {
+            state.package.name = data.my_package_info.package_name
+        }
+
+    }
+
 }
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -32,7 +32,7 @@ export default {
 
                     resolve(true)
 
-                }).catch(error => reject(error))
+                }).catch(() => reject(false))
 
             })
         }
