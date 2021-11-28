0281e129b20b5f51e91a21d47146432abd1c889f
Shyamin Ayesh <me@shyamin.com>
2021-11-28 22:29:17 +0530

    cleanup defaults

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router: router,
+	store: store,
+	render: h => h(App)
 }).$mount('#app')
